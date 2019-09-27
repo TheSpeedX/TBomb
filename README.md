4f06921552abcb9f3e5d86bfd9de78abbacab3b6
TheSpeedX <ggspeedx29@gmail.com>
2019-09-27 11:17:01 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,88 @@
+# TBomb
+This is a SMS Bomber for Debian Based Linux And Termux..
+
+This Script is Only For Educational Purposes or To Prank.
+ Do not Use This To Harm Others.
+ I Am Not Responsible For The Misuse Of The Script.
+
+The Script Requires A working Net Connection To Work..
+No Balance will be deducted for using this script to send SMS...
+
+While Using The Infinite Bomb Use 2-3 Seconds Delay And 10 to 20 Threads For Maximum Performance...
+<br>Don't Put Spaces In Between Number Ex- 9999999999
+
+ Make Sure To Update it for New Versions...
+
+ That's All !!!
+
+Make Sure You Are Using python3
+
+## Usage
+Run These Commands To Run TBomb<br>
+### For Termux
+To USE the Script Type The Following Commands in Termux...
+
+```
+apt install git
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x TBomb.sh
+./TBomb.sh
+```
+
+### For Linux (Debian Based)
+
+To USE the Script Type The Following Commands in Linux Terminal...
+
+```
+sudo apt install git
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x TBomb.sh
+sudo bash TBomb.sh
+```
+
+Now the Script Will Execute..
+
+## FEATURES 
+
+ [+] Lots Of APIs <br>
+ [+] Unlimited And Super-Fast Bombing <br>
+ [+] International Bombing Available <br>
+ [+] Call Bombing <br>
+ [+] Protection List <br>
+ [+] Updated Frequently <br>
+ [+] Automated Future Updates <br>
+ [+] Easy To Use And Embed in Code <br>
+ 
+## DEMO
+<br>
+Watch Indian Bombing Method <a href="https://youtu.be/9KWkwsr_QGw">here</a> <br><br>
+Watch International Bombing Method <a href="https://youtu.be/JqsHkyIcnPM">here</a> <br><br>
+
+# CONTRIBUTORS
+<br><br>
+[*]  SpeedX<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[-] Mail At: ggspeedx29@gmail.com  <br>
+[*]  The Black Hacker Roxstar<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[-] Ping At: http://wa.me/917600140353 <br>
+[*]  Rieltar<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[-] Ping At: https://t.me/Rieltar  <br>
+[*]  0n1cOn3 (Stefan)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[-] Mail At: 0n1cOn3@gmx.ch <br>
+
+## Donators
+<br><br>
+[@] 34D30Y ( 34db0y@protonmail.com  )
+<br>
+[@] SC AMAN
+<br><br>
+# CONTACT
+For Any Queries Join Us On WhatsApp!!!
+          Group Link: http://bit.do/speedxgit
+  <a href="http://bit.do/speedxgit">Join My Group</a>
+
+           Mail: ggspeedx29@gmail.com
+
+           YouTube Channel: https://www.youtube.com/c/GyanaTech
+  <a href="https://www.youtube.com/c/GyanaTech">Check My Channel</a>
---
+++ TBomb.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+clear
+echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m TBomb \e[0m"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.speedx ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python-lzma
+apt install python3-lzma
+apt install python3-pip
+pip3 install urllib3
+pip3 install requests
+echo This Script Was Made By SpeedX >update.speedx
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet TBomb
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border SpeedX
+echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo " "
+echo "Press 1 To  Start SMS Bomber "
+echo "Press 2 To  Start Call Bomber "
+echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
+echo "Press 4 To  Protect Your Number "
+echo "Press 5 To  View Features "
+echo "Press 6 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By SpeedX'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/TheSpeedX/TBomb
+if [[ -s TBomb/TBomb.sh ]];then
+cd TBomb
+cp -r -f * .. > temp
+cd ..
+rm -rf  TBomb >> temp
+rm update.speedx >> temp
+rm temp
+chmod +x TBomb.sh
+fi
+echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./TBomb.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Protect List By SpeedX'> protect.xxx
+python3 bomber.py protect
+rm *.xxx >/dev/null 2>&1
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;33m"
+figlet TBomb
+echo -e "\e[1;34mCreated By \e[1;34m"
+toilet -f mono12 -F border SpeedX
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo "  [+] Unlimited And Super-Fast Bombing"
+echo "  [+] International Bombing"
+echo "  [+] Call Bombing "
+echo "  [+] Protection List"
+echo "  [+] Automated Future Updates"
+echo "  [+] Easy To Use And Embed in Code"
+echo -e "\e[1;32m                   Contributors\e[1;33m"
+echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
+echo "         [-] Mail At: ggspeedx29@gmail.com"
+echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
+echo "         [-] Ping At: http://wa.me/917600140353"
+echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
+echo "         [-] Ping At: https://t.me/Rieltar"
+echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
+echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+echo ""
+echo ""
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+elif [ $ch -eq 6 ];then
+clear
+echo -e "\e[1;31m"
+figlet TBomb
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border SpeedX
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo " "
+exit 0
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+fi
+done
