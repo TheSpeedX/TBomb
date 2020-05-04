12b6058a33a9272b8c8cd28383ec7c0200cf2305
mrsujal88 <64422738+mrsujal88@users.noreply.github.com>
2020-05-04 16:22:58 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -836,11 +836,11 @@ try:
 except Exception:
     type = 0
 if type == 1:
-    nm = int(input("Enter Number of Calls To Send(Maximum 15): "))
-    if nm > 15:
+    nm = int(input("Enter Number of Calls To Send(Maximum 1000): "))
+    if nm > 1000:
         print("\t\tYou Have Entered " + str(nm) +
-              ".\n\tNormalizing Value To 15")
-        nm = 15
+              ".\n\tNormalizing Value To 1000")
+        nm = 1000
     dl = float(input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 elif type == 0:
     if cc == "91":
@@ -853,9 +853,9 @@ elif type == 0:
             input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 maxlim = 0
 if cc == "91":
-    maxlim = 500
+    maxlim = 50000
 else:
-    maxlim = 100
+    maxlim = 10000
 if nm > maxlim:
     print('\n\n\tSorry Due To Misuse Of This Script We Only Provide ' +
           str(maxlim) + ' SMS At Once...\n\n')
