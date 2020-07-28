57ce6837f205e4c4a46b948865605cd625b38146
TheSpeedX <ggspeedx29@gmail.com>
2020-07-28 17:53:38 +0530

    Update bash code cleaned

--- TBomb.sh
+++ TBomb.sh
@@ -1,133 +1,110 @@
 #!/bin/bash
-clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
-echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
-echo "Press Enter To Continue"
-read a1
-if [[ -s update.speedx ]];then
-echo "All Requirements Found...."
+
+detect_os() {
+    if [ -e $PREFIX/bin/termux-info ]; then
+        OS="TERMUX"
+    else
+        OS="LINUX"
+    fi
+}
+
+pause() {
+    read -n1 -r -p "Press any key to continue..." key
+}
+banner() {
+    clear
+    echo -e "\e[1;31m"
+    if ! [ -x "$(command -v figlet)" ]; then
+        echo 'Introducing TBomb'
+    else
+        figlet TBomb
+    fi
+    if ! [ -x "$(command -v toilet)" ]; then
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+    else
+        echo -e "\e[1;34mCreated By \e[1;34m"
+        toilet -f mono12 -F border SpeedX
+    fi
+    echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+    echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/GyanaTech \e[0m"
+    echo " "
+    pause
+}
+
+init_environ(){
+    if [ $OS = "TERMUX" ]; then
+        PYTHON="python"
+        PIP="python -m pip"
+        PKG="pkg"
+    elif [ $OS = "LINUX" ]; then
+        PYTHON="python3"
+        PIP="python3 -m pip"
+        PKG="apt"
+    fi
+}
+
+install_deps(){
+    $PKG install -y figlet
+    $PKG install -y curl
+    $PKG install -y openssl
+    $PKG install -y git
+    $PKG install -y $PYTHON
+    $PKG install -y $PYTHON-pip
+    $PIP install -r requirements.txt    
+}
+
+banner
+detect_os
+init_environ
+if [ -e .update ];then
+    echo "All Requirements Found...."
 else
-echo 'Installing Requirements....'
-echo .
-echo .
-apt install figlet toilet python curl -y
-apt install python3-pip
-pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
-echo Requirements Installed....
-echo Press Enter To Continue...
-read upd
+    echo 'Installing Requirements....'
+    echo .
+    echo .
+    install_deps
+    echo This Script Was Made By SpeedX > .update
+    echo 'Requirements Installed....'
+    pause
 fi
 while :
 do
-rm *.xxx >/dev/null 2>&1
-clear
-echo -e "\e[1;31m"
-figlet TBomb
-echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
-echo " "
-echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
-echo " "
-echo "Press 1 To  Start SMS Bomber "
-echo "Press 2 To  Start Call Bomber "
-echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
-echo "Press 4 To  View Features "
-echo "Press 5 To  Exit "
-read ch
-if [ $ch -eq 1 ];then
-clear
-echo -e "\e[1;32m"
-rm *.xxx >/dev/null 2>&1
-python3 bomber.py
-rm *.xxx >/dev/null 2>&1
-exit 0
-elif [ $ch -eq 2 ];then
-clear
-echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
-python3 bomber.py call
-rm *.xxx >/dev/null 2>&1
-exit 0
-elif [ $ch -eq 3 ];then
-clear
-apt install git -y
-echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
-cp -r -f * .. > temp
-cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
-rm temp
-chmod +x TBomb.sh
-fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
-echo -e "\e[1;32m All The Required Packages Will Be Installed..."
-echo -e "\e[1;34m Press Enter To Proceed To Restart..."
-read a6
-./TBomb.sh
-exit
-elif [ $ch -eq 4 ];then
-clear
-echo -e "\e[1;33m"
-figlet TBomb
-echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
-echo  " "
-echo -e "\e[1;32m                   Features\e[1;34m"
-echo "  [+] Unlimited And Super-Fast Bombing"
-echo "  [+] International Bombing"
-echo "  [+] Call Bombing "
-echo "  [+] Protection List"
-echo "  [+] Automated Future Updates"
-echo "  [+] Easy To Use And Embed in Code"
-echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
-echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Rieltar"
-echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
-echo ""
-echo ""
-echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
-echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
-echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
-echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
-echo  " "
-echo -e "\e[4;31m That's All !!!\e[0m"
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
-echo "Press Enter To Go Home"
-read a3
-clear
-elif [ $ch -eq 5 ];then
-clear
-echo -e "\e[1;31m"
-figlet TBomb
-echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
-echo " "
-exit 0
-else
-echo -e "\e[4;32m Invalid Input !!! \e[0m"
-echo "Press Enter To Go Home"
-read a3
-clear
-fi
+    banner
+    echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+    echo " "
+    echo "Press 1 To  Start SMS  Bomber "
+    echo "Press 2 To  Start CALL Bomber "
+    echo "Press 3 To  Start MAIL Bomber "
+    echo "Press 4 To  Update (Works On Linux And Linux Emulators) "
+    echo "Press 5 To  Exit "
+    read ch
+    clear
+    if [ $ch -eq 1 ];then
+        $PYTHON bomber.py --sms
+        exit
+    elif [ $ch -eq 2 ];then
+        $PYTHON bomber.py --call
+        exit
+    elif [ $ch -eq 3 ];then
+        $PYTHON bomber.py --mail
+        exit
+    elif [ $ch -eq 4 ];then
+        echo -e "\e[1;34m Downloading Latest Files..."
+        rm -f .update
+        git reset --hard HEAD
+        git pull --force
+        echo -e "\e[1;32m TBomb Has Been Updated..."
+        echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+        echo -e "\e[1;34m RUNNING TBomb Again..."
+        pause
+        ./TBomb.sh
+        exit
+    elif [ $ch -eq 5 ];then
+        banner
+        exit
+    else
+        echo -e "\e[4;32m Invalid Input !!! \e[0m"
+        pause
+    fi
 done
