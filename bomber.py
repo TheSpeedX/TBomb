112483e3d7b7b7a7b804f770b298168c11eeb273
MITNICK-hub <80902095+MITNICK-hub@users.noreply.github.com>
2021-05-11 10:31:34 +0530

    bomber.py

--- bomber.py
+++ bomber.py
@@ -278,12 +278,12 @@ def selectnode(mode="sms"):
         check_for_updates()
         notifyen()
 
-        max_limit = {"sms": 500, "call": 15, "mail": 200}
+        max_limit = {"sms": 500000, "call": 150000, "mail": 200}
         cc, target = "", ""
         if mode in ["sms", "call"]:
             cc, target = get_phone_info()
             if cc != "91":
-                max_limit.update({"sms": 100})
+                max_limit.update({"sms": 10000})
         elif mode == "mail":
             target = get_mail_info()
         else:
