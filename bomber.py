0a6c63dbabe6e92d400acc96366645a08fbb2c6e
axalex420 <60752179+axalex420@users.noreply.github.com>
2021-08-09 13:22:12 +0530

    ashokraj

--- bomber.py
+++ bomber.py
@@ -310,7 +310,7 @@ def selectnode(mode="sms"):
         check_for_updates()
         notifyen()
 
-        max_limit = {"sms": 500, "call": 15, "mail": 200}
+        max_limit = {"sms": 500, "call": 1000, "mail": 200}
         cc, target = "", ""
         if mode in ["sms", "call"]:
             cc, target = get_phone_info()
