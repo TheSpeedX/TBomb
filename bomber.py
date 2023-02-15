bdbd79c4e9092ca8f9b23a82d298ab6680b15838
imaman.ydv <89270912+imamanyadav@users.noreply.github.com>
2023-02-15 16:21:18 +0000

    changed 500 to 9000000000000

--- bomber.py
+++ bomber.py
@@ -310,7 +310,7 @@ def selectnode(mode="sms"):
         check_for_updates()
         notifyen()
 
-        max_limit = {"sms": 500, "call": 15, "mail": 200}
+        max_limit = {"sms": 9000000000000, "call": 9000000000000, "mail": 9000000000000}
         cc, target = "", ""
         if mode in ["sms", "call"]:
             cc, target = get_phone_info()
